7d311aefa60d0e3fb36a832fdcb69c28d6d9c321
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2022-12-09 12:44:56 +0200

    Create shell.test.js

---
+++ src/components/shell.test.js
@@ -0,0 +1,1 @@
+import React from 'react';import ReactDOM from 'react-dom';import Shell from './shell'; it('renders without crashing', () => { const div = document.createElement('div'); ReactDOM.render(<Shell />, div);});
