1c1d98e7589072eb4e554e0e1330ac2fece00170
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2022-12-09 12:42:38 +0200

    Create shell.js

---
+++ src/components/shell.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Grid, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router';
+import Home from './home';
+import './shell.css';
+
+/**
+ * The (App) Shell contains the web app's entire UI.
+ *
+ * The navigation bar is always displayed, with either a `Home` or `Article` component beneath it.
+ */
+export default class Shell extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {'documents': []};
+  }
+
+  componentDidMount() {
+    fetch('/documents').then(response => {
+      if (response.status !== 200) {
+        console.log('AMP document list fetch failed with code: ' + response.status);
+        return;
+      }
+      response.json().then(data => {
+        this.setState({'documents': data});
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to='/'>The Scenic</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+
+        <Grid className='contents'>
+          { this.props.children || <Home documents={this.state.documents} /> }
+        </Grid>
+      </div>
+    );
+  }
+}
